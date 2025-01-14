89ed3a8c0b6a6d04ab1e596d20f96f1874f5bae6
Durelle Abdul <Durelle.abdul.07@gmail.com>
2025-01-14 11:59:09 +0200

    spring initialisation

---
+++ WorkingOnit/src/main/java/com/Deisha/WorkingOnit/WorkingOnitApplication.java
@@ -0,0 +1,13 @@
+package com.Deisha.WorkingOnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingOnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingOnitApplication.class, args);
+	}
+
+}
---
+++ WorkingOnit/src/test/java/com/Deisha/WorkingOnit/WorkingOnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Deisha.WorkingOnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingOnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
